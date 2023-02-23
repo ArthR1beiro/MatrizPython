a7c3b5d07b1127e606713fb904f2c2a6cca33e69
Arthur Ribeiro Aguiar <arthurribeiro495@gmail.com>
2023-02-23 15:45:46 -0300

    add .py

---
+++ matriz.py
@@ -0,0 +1,41 @@
+matriz = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+matrizinf = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+matrizsup = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+for l in range (0, 4):
+    for c in range (0, 4):
+        matriz[l][c] = int(input(f'Digite um valor para a{l+1}{c+1}: '))
+        matrizsup[l][c] = matriz[l][c]
+        matrizinf[l][c] = matriz[l][c]
+
+
+
+print('-='*30)
+print("Matriz Quadrada")
+for l in range (0, 4):
+    for c in range (0, 4):
+        print(f'[{matriz[l][c]}]', end='')
+    print()
+
+print('-='*30)
+for l in range (0, 4):
+    for c in range (0, 4):
+        if(l>c):
+            matrizinf[l][c] = 0
+
+print("Matriz Triangular Inferior")
+for l in range (0, 4):
+    for c in range (0, 4):
+        print(f'[{matrizinf[l][c]}]', end='')
+    print()
+
+print('-='*30)
+for l in range (0, 4):
+    for c in range (0, 4):
+        if(c>l):
+            matrizsup[l][c] = 0
+
+print("Matriz Triangular Superior")
+for l in range (0, 4):
+    for c in range (0, 4):
+        print(f'[{matrizsup[l][c]}]', end='')
+    print()
