34e7ebdc98e88b54f737c24c234066cef14338a4
Arthur Ribeiro Aguiar <arthurribeiro495@gmail.com>
2023-02-24 17:37:36 -0300

    update prints

--- matriz.py
+++ matriz.py
@@ -10,7 +10,7 @@ for l in range (0, 4):
 
 
 print('-='*30)
-print("Matriz Quadrada")
+print("Matriz Quadrada\n")
 for l in range (0, 4):
     for c in range (0, 4):
         print(f'[{matriz[l][c]}]', end='')
@@ -22,20 +22,22 @@ for l in range (0, 4):
         if(l>c):
             matrizinf[l][c] = 0
 
-print("Matriz Triangular Inferior")
+print("Matriz Triangular Superior\n")
 for l in range (0, 4):
     for c in range (0, 4):
-        print(f'[{matrizinf[l][c]}]', end='')
+        print(f'[{matrizsup[l][c]}]', end='')
     print()
 
 print('-='*30)
 for l in range (0, 4):
     for c in range (0, 4):
         if(c>l):
-            matrizsup[l][c] = 0
+            matrizinf[l][c] = 0
 
-print("Matriz Triangular Superior")
+print("Matriz Triangular Inferior\n")
 for l in range (0, 4):
     for c in range (0, 4):
-        print(f'[{matrizsup[l][c]}]', end='')
+        print(f'[{matrizinf[l][c]}]', end='')
     print()
+
+print("\nPrograma encerrado, obrigado por ultilizar.")
