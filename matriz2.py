4c3e33aa90e2787f5d659b2c3c72ed7a4ccf53b0
Arthur Ribeiro Aguiar <arthurribeiro495@gmail.com>
2023-02-28 22:21:13 -0300

    update custom matriz

--- matriz2.py
+++ matriz2.py
@@ -1,32 +1,46 @@
-matriz = []
+linhasA = int(input("Digite o número de linhas da Matriz A: "))
+colunasA = int(input("Digite o número de colunas da Matriz A: "))
 
-for l in range (0, 4):
-    for c in range (0, 4):
-        matriz[l][c] = int(input(f'Digite um valor para a{l+1}{c+1}: '))
+matrizA = []
+
+for i in range(linhasA):
+    linhaA = []
+    for j in range(colunasA):
+        linhaA.append(0)
+    matrizA.append(linhaA)
+
+
+linhasB = int(input("Digite o número de linhas da Matriz B: "))
+colunasB = int(input("Digite o número de colunas da Matriz B: "))
+
+matrizB = []
+
+for i in range(linhasB):
+    linhaB = []
+    for j in range(colunasB):
+        linhaB.append(0)
+    matrizB.append(linhaB)
+
+
+
+# for l in range (0, 4):
+#     for c in range (0, 4):
+#         matriz[l][c] = int(input(f'Digite um valor para a{l+1}{c+1}: '))
     
 
 print('-='*30)
-print("Matriz Quadrada\n")
-for l in range (0, 4):
-    for c in range (0, 4):
-        print(f'[{matriz[l][c]}]', end='')
+print(f"Matriz A {linhasA}x{colunasA}\n")
+for l in range (linhasA): 
+    for c in range (colunasA):
+        print(f'[{matrizA[l][c]}]', end='')
+    print()
+
+print('-='*30)
+print(f"Matriz B {linhasB}x{colunasB}\n")
+for l in range (linhasB):
+    for c in range (colunasB):
+        print(f'[{matrizB[l][c]}]', end='')
     print()
 
 
-if (rowA == colB):
-    print("É possivel realizar a multiplicação")
-else:
-    print("Não é possivel realizar a multiplicação")
     
-# linhas = int(input("Digite o número de linhas: "))
-# colunas = int(input("Digite o número de colunas: "))
-
-# matriz = []
-
-# for i in range(linhas):
-#     linha = []
-#     for j in range(colunas):
-#         linha.append(0)
-#     matriz.append(linha)
-
-# print(matriz)
