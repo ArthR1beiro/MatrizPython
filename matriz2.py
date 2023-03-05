bc2079e15ba3d477f9aa96f187b50e3a1f383abf
Arthur Ribeiro Aguiar <arthurribeiro495@gmail.com>
2023-03-04 21:19:37 -0300

    finish matriz2

--- matriz2.py
+++ matriz2.py
@@ -9,17 +9,6 @@ while colunasA < 2:
     print("\nO numero deve ser Igual ou maior que 2")
     colunasA = int(input("Digite o número de colunas da Matriz A: "))
     
-linhasB = int(input("\nDigite o número de colunas da Matriz A: "))
-while linhasB < 2:
-    print("\n(O numero deve ser Igual ou maior que 2)")
-    linhasB = int(input("Digite o número de linhas da Matriz B: "))
-
-colunasB = int(input("\nDigite o número de colunas da Matriz B: "))
-while colunasB < 2:
-    print("\nO numero deve ser Igual ou maior que 2")
-    colunasB = int(input("Digite o número de colunas da Matriz B: "))
-
-
 matrizA = []
 
 for i in range(linhasA):
@@ -28,40 +17,35 @@ for i in range(linhasA):
         linhaA.append(0)
     matrizA.append(linhaA)
 
-matrizB = []
-
-for i in range(linhasB):
-    linhaB = []
-    for j in range(colunasB):
-        linhaB.append(0)
-    matrizB.append(linhaB)
 
 
-if (linhasA == colunasB):
-    print("É possivel realizar a soma")
-    print('-='*30)
+print('-='*30)
+print("Digite os valores para Matriz A\n")
+for l in range (linhasA):
+    for c in range (colunasA):
+          matrizA[l][c] = int(input(f'Digite um valor para a{l+1}{c+1}: '))
 
-    print("Digite os valores para Matriz A\n")
-    for l in range (linhasA):
-        for c in range (colunasA):
-            matrizA[l][c] = int(input(f'Digite um valor para a{l+1}{c+1}: '))
-            
-    print("\nDigite os valores para Matriz B\n")
-    for l in range (linhasB):
-        for c in range (colunasB):
-            matrizB[l][c] = int(input(f'Digite um valor para b{l+1}{c+1}: '))
 
-    print(f"Matriz A {linhasA}x{colunasA}\n")
-    for l in range (linhasA): 
-        for c in range (colunasA):
-            print(f'[{matrizA[l][c]}]', end='')
-        print()
+print('-='*30)            
+print(f"Matriz A {linhasA}x{colunasA}\n")
+for l in range (linhasA): 
+    for c in range (colunasA):
+         print(f'[{matrizA[l][c]}]', end='')
+    print()
 
-    print('-='*30)
-    print(f"Matriz B {linhasB}x{colunasB}\n")
-    for l in range (linhasB):
-        for c in range (colunasB):
-            print(f'[{matrizB[l][c]}]', end='')
-        print()
-else:
-    print("\nNão é possivel realizar a soma")
+print('-='*30)
+print(f"Matriz A oposta {linhasA}x{colunasA}\n")
+for l in range (linhasA): 
+    for c in range (colunasA):
+         print(f'[{-matrizA[l][c]}]', end='')
+    print()
+
+print('-='*30)
+print(f"Matriz A transposta {linhasA}x{colunasA}\n")
+for l in range (linhasA): 
+    for c in range (colunasA):
+         print(f'[{matrizA[c][l]}]', end='')
+    print()
+
+    
+
